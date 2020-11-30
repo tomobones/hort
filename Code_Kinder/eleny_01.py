95ed847e2b3b329875f78eda71632db95f95149c
tomobones <thomas.vogg@posteo.de>
2020-11-30 19:46:12 +0100

    added/changed code of kids

---
+++ Code_Kinder/eleny_01.py
@@ -0,0 +1,8 @@
+import turtle
+t=turtle.Pen()
+turtle.bgcolor('black')
+t.pencolor('white')
+t.width(5)
+t.forward(100)
+
+turtle.Screen().exitonclick()
---
+++ Code_Kinder/wenzai_02.py
@@ -0,0 +1,6 @@
+import turtle 
+
+stift=turtle.Pen()
+for  strecke  in  range(100):
+     stift.forward(strecke)
+     stift.left(90)
