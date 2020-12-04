2542f60b798bb34e051c4a577d32cdde43ae84cc
tomobones <thomas.vogg@posteo.de>
2020-12-04 14:20:33 +0100

    creating/playing with python code

--- Code_Kinder/aurelia_01.py
+++ Code_Kinder/aurelia_01.py
@@ -1,2 +1,36 @@
-name = input("hallo! wie heißt du? ")
-print("hallo " + name + "!")
+import turtle
+t = turtle.Pen()
+turtle.bgcolor("black")
+t.pencolor("white")
+t.width(5)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.forward(50)
+t.left(120)
+t.forward(100)
+t.forward(123)
+t.pencolor("blue")
+t.left(100)
+t.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.Screen().exitonclick()
--- Code_Kinder/elias_01.py
+++ Code_Kinder/elias_01.py
@@ -1,20 +1,48 @@
 import turtle
 stift=turtle.Pen()
-turtle.bgcolor('gold')
+turtle.bgcolor('black')
+stift.width(5)
 
-for x in range(1234567890):
-	stift.forward(x)
-	stift.left(200)
-	stift.pencolor('green')
-	stift.forward(100)
-	stift.left(200)
-	stift.pencolor('blue')
-	stift.forward(100)
-	stift.left(200)
+stift.pencolor('blue')
+
+stift.forward(100)
+stift.left(90)
+
+stift.pencolor('green')
+stift.forward(100)
+stift.left(90)
+
+stift.pencolor('yellow')
+stift.forward(100)
+stift.left(90)
+
+stift.pencolor('brown')
+stift.forward(110)
+stift.left(90)
+
+stift.pencolor('purple')
+stift.forward(110)
+stift.left(90)
+
+stift.pencolor('orange')
+stift.forward(120)
+stift.left(90)
+
+stift.pencolor('red')
+stift.forward(120)
+stift.left(90)
+
+for wert in range(1,100):
+	stift.forward(100+wert)
+	stift.left(20+wert)
+        stift.pencolor('green')
+        stift.forward(100+wert)
+        stift.left(20+wert)
+        stift.pencolor('blue')
+	stift.forward(100+wert)
+        stift.left(20+wert)
 	stift.pencolor('purple')
-	stift.forward(100)
-	stift.left(200)
-	stift.pencolor('cyan')
-	stift.forward(x)
-	stift.left(30)
+        stift.forward(100+wert)
+        stift.left(20+wert)
+
 turtle.Screen().exitonclick()
