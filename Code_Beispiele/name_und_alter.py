e4b611aebfd071cc9d409010185a9ee50e6b5541
Thomas Vogg <thomas.vogg@posteo.de>
2020-12-07 12:22:17 +0100

    fixed bugs: casting and inconsistence in variables

--- Code_Beispiele/name_und_alter.py
+++ Code_Beispiele/name_und_alter.py
@@ -1,5 +1,5 @@
 name_string = input("Hallo! Wie heißt du? ")
-alter_string = input("Hallo " + name + "! Wieviele Jahre bist du alt? ")
+alter_string = input("Hallo " + name_string + "! Wieviele Jahre bist du alt? ")
 
 alter = int(alter_string)
 
@@ -8,7 +8,7 @@ alter_stunden = alter_tage * 24
 alter_minuten = alter_stunden * 60
 alter_sekunden = alter_minuten * 60
 
-print(name + ", du bist also " + str(alter) + " Jahre alt.")
+print(name_string + ", du bist also " + str(alter) + " Jahre alt.")
 print("Das sind ungefähr ", str(alter_tage), " Tage.")
 print("Das sind ungefähr " + str(alter_stunden) + " Stunden.")
 print("Das sind ungefähr " + str(alter_minuten) + " Minuten.")
