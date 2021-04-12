069178fa62341c99a644263f3db8486e2777cfa6
Thomas Vogg <thomas.vogg@posteo.de>
2021-04-12 16:12:28 +0200

    Hanna was coding!

--- Code_Kinder/hanna_02.py
+++ Code_Kinder/hanna_02.py
@@ -1,12 +1,12 @@
 import turtle
 
-colors=['khaki','thistle']
+colors=['slate blue','steel blue','hot pink','light pink']
 t=turtle.Pen()
 turtle.bgcolor('black')
 for x in range(360):
-    t.pencolor(colors[x%2])
+    t.pencolor(colors[x%4])
     t.width(x/100+1)
     t.forward(x)
-t.left(67)
+    t.left(80)
 
 turtle.Screen().exitonclick()
