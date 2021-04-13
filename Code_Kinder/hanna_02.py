01570eb2bb1e90e1b80b5f1546d8bba7a12b7cc6
Thomas Vogg <thomas.vogg@posteo.de>
2021-04-13 15:08:41 +0200

    Hanna programmiert.

--- Code_Kinder/hanna_02.py
+++ Code_Kinder/hanna_02.py
@@ -1,10 +1,10 @@
 import turtle
 
-colors=['slate blue','steel blue','hot pink','light pink']
+colors=['deep sky blue','snow','spring green','brown4','pink3','purple1']
 t=turtle.Pen()
 turtle.bgcolor('black')
 for x in range(360):
-    t.pencolor(colors[x%4])
+    t.pencolor(colors[x%6])
     t.width(x/100+1)
     t.forward(x)
     t.left(80)
