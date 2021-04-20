a5ecfa5f6a5092a671a51a644d9f35575e2418bf
Thomas Vogg <thomas.vogg@posteo.de>
2021-04-20 15:04:46 +0200

    Leopold is coding.

---
+++ Code_Kinder/teodora_01.py
@@ -0,0 +1,11 @@
+import turtle
+
+colors=['cyan', 'purple', 'blue']
+t=turtle.Pen()
+turtle.bgcolor('black')
+for x in range(360):
+    t.pencolor(colors[x%3])
+    t.width(x/100+1)
+    t.forward(x)
+    t.left(59)
+turtle.Screen().exitonclick()
