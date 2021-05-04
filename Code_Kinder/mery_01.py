a32306e4b136a9dff6bfd67e08393cef2e5bb6f4
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-04 15:18:16 +0200

    Mery is coding

--- Code_Kinder/mery_01.py
+++ Code_Kinder/mery_01.py
@@ -1,19 +1,34 @@
-import tortel
+import turtle
 import random
 
-pen=turtel.Turtel()
-colors=["red", "red2", "red3", "tomato", "tomato2", "tomato3", "tomato4"]
+turtle.bgcolor("gray1")
 
+pen = turtle.Turtle()
+colors = ["red", "red2", "firebrick1", "DeepPink2", "tomato", "magenta2", "deep pink", "maroon1"]
+  
 def kurve(größe):
     for i in range(200):
         pen.right(1)
         pen.forward(.1 * größe)
-
+  
 def herz(x, y, größe, color):
-    pen.up()
+    pen.up() 
     pen.setpos(x, y)
     pen.down()
 
     pen.fillcolor(color)
     pen.begin_fill()
     pen.left(140)
+    pen.forward(11.2 * größe + 1)
+    kurve(größe)
+    pen.left(120)
+    kurve(größe)
+    pen.forward(11.2 * größe)
+    pen.end_fill()
+    pen.right(220)
+  
+for x in range(100):
+    herz(random.randint(-400, 400), random.randint(-300,300), random.randint(2,11), colors[random.randint(0, 7)])
+
+pen.ht()
+turtle.Screen().exitonclick()
