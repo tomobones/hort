3493c58248bf49645b77da1cab54ad7d98b47e52
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-04 13:35:34 +0200

    Teodora is coding

--- Code_Kinder/teodora_01.py
+++ Code_Kinder/teodora_01.py
@@ -1,10 +1,10 @@
 import turtle
 
-colors=['cyan', 'purple', 'blue']
+colors=['cyan', 'purple', 'blue', 'PaleGreen1', 'lavender', 'MistyRose2','gray8']
 t=turtle.Pen()
 turtle.bgcolor('black')
 for x in range(360):
-    t.pencolor(colors[x%3])
+    t.pencolor(colors[x%7])
     t.width(x/100+1)
     t.forward(x)
     t.left(59)
