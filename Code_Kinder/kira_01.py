e48eaf69f6a8c6daf701141cd7449a3ba5f9f5e7
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-06 15:22:33 +0200

    Kira is coding

--- Code_Kinder/kira_01.py
+++ Code_Kinder/kira_01.py
@@ -1,11 +1,11 @@
 import turtle
 
-colors=[ 'cyan4','cyan2','green2']
+colors=[ 'purple','hot pink','deep pink','pink','pale violet pink','maroon','mediom violet pink','medium ochid']
 t=turtle.Pen()
-turtle.bgcolor('pink')
+turtle.bgcolor('cyan2')
 for x in range(333):
     t.pencolor(colors[x%3])
-    t.width(22+44)
+    t.width(222+45454)
     t.forward(x)
     t.left(33)
 turtle.Screen().exitonclick()
