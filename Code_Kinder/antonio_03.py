e1c2cdf94d2def4853fde223e91f30c03db9b3e4
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-11 16:13:35 +0200

    Toni is coding for father's day

---
+++ Code_Kinder/antonio_03.py
@@ -0,0 +1,35 @@
+import turtle
+def stern_klein():
+    pen.begin_fill()
+    for i in range(5):
+        pen.color("blue")
+        pen.forward(5)
+        pen.left(144)
+    pen.end_fill()
+
+def stern_mittel():
+    for i in range(5):
+        for j in range(10):
+            stern_klein()
+            pen.up()
+            pen.forward(5)
+            pen.down()
+        pen.left(144)
+
+def stern_gross():
+    for i in range(5):
+        for j in range(10):
+            stern_mittel()
+            pen.up()
+            pen.forward(50)
+            pen.down()
+        pen.left(144)
+
+pen = turtle.Turtle()
+pen.up()
+pen.setpos(-300,0)
+pen.down()
+stern_gross()
+
+pen.ht()
+turtle.Screen().exitonclick()
