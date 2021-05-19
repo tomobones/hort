8b75ec15c6e8d2b88fe2e84a9d7aed17ddf7bca1
Thomas Vogg <thomas.vogg@posteo.de>
2021-05-19 15:15:22 +0200

    Toni is coding

--- Code_Kinder/antonio_01.py
+++ Code_Kinder/antonio_01.py
@@ -1,11 +1,13 @@
 import  turtle
 
-colors = ['purple', 'red', 'blue', 'gray', 'yellow', 'orange']
+colors = ['red', 'blue']
 
 t=turtle.Pen()
 turtle.bgcolor('black')
 for x in range(400):
-    t.pencolor(colors[x%6])
+    t.pencolor(colors[x%2])
     t.width(x/100+1)
     t.forward(x)
-    t.left(60)
+    t.left(90)
+
+turtle.Screen().exitonclick()
